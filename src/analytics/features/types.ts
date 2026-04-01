6df9cc136ca03ab8c7cde8c995ac85c93043a486
DS Boyce <260543580+ds-boyce@users.noreply.github.com>
2026-04-01 14:47:13 -0700

    Feedback

--- src/analytics/features/types.ts
+++ src/analytics/features/types.ts
@@ -13,7 +13,7 @@ export enum Features {
   ImageUploadsBlobSize2mbEnabled = 'image_uploads:blob_size_2mb:enabled',
   GroupChatsEnable = 'group_chats:enable',
   DmsNewMessageComposerEnable = 'dms:new_message_composer:enable',
-  NativeLanguageDetectionDisable = 'native_language_detection:disable',
+  NativeLanguageDetectionEnable = 'native_language_detection:enable',
 
   AATest = 'aa-test',
 }
